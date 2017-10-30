a617a7abad0b99ba4bccaa33eb5db552d07a3651
mewild <michael@wildapplications.com>
2017-10-30 11:01:17 +0000

    New product proto

--- src/models/product.schema.js
+++ src/models/product.schema.js
@@ -12,20 +12,12 @@ function getPrice(p){
 var schema = new mongoose.Schema({
   name : { type : String, required : true },
   description: { type: String, required: false },
-  price: {type: Number, required: true, default: 0, get: getPrice, set: setPrice},
+  price: {type: Number, required: true, default: 0},
   owner: { type : Number, required : true },
   in_stock: { type: Boolean, required: true, default: true},
   age_restricted: { type: Boolean, required: true, default: false}
 }, {
   timestamps: true
-});
-
-schema.path('price').set(function(p){
-  return p * 100;
-});
-
-schema.path('price').get(function(p){
-  return parseFloat((p/100).toFixed(2));
 });
 
 schema.set('toJSON', {getters: true, setters:true});
